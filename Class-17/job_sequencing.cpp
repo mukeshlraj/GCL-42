e5cf06eab45ecc8df49f2f80550219af201ad0d0
Mukesh Raj <mkrj@amazon.com>
2022-07-03 16:08:57 +0530

    Class 17

--- Class-17/job_sequencing.cpp
+++ Class-17/job_sequencing.cpp
@@ -3,3 +3,56 @@
 
 using namespace std;
 
+class Job {
+public:
+    int id;
+    int deadline;
+    int profit;
+
+    Job(int id, int deadline, int profit) {
+        this->id = id;
+        this->deadline = deadline;
+        this->profit = profit;
+    }
+};
+
+bool comp(Job &a, Job &b) {
+    return a.profit > b.profit;
+}
+
+
+// TC : O(n*log(n) + n*max_deadline) = O(n * (log(n) + max_deadline))
+
+pair<int, int> job_sequencing(vector<Job> jobs) {
+    int n = jobs.size();
+
+    sort(jobs.begin(), jobs.end(), comp);
+
+    int max_deadline = jobs[0].deadline;
+    for (int i = 1; i < n; ++i) {
+        max_deadline = max(max_deadline, jobs[i].deadline);
+    }
+
+    vector<bool> slots(max_deadline, false);
+    
+    int job_count = 0, max_profit = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = jobs[i].deadline - 1; j >= 0; --j) {
+            if (slots[j] == false) {
+                slots[j] = true;
+                job_count++;
+                max_profit += jobs[i].profit;
+                break;
+            }
+        }
+    }
+
+    return {job_count, max_profit};
+}
+
+int main() {
+    pair<int, int> res = job_sequencing({{1,2,100},{2,1,19},{3,2,27},{4,1,25},{5,1,15}});
+
+    cout<<res.first<<" "<<res.second<<endl;
+}
--- Class-17/meeting_rooms.cpp
+++ Class-17/meeting_rooms.cpp
@@ -3,3 +3,40 @@
 
 using namespace std;
 
+// TC : O(n * log(n))
+// Aux Space : O(n)
+
+bool comp(pair<int, int> &a, pair<int, int> &b) {
+
+    if (a.first == b.first)
+        return a.second < b.second;
+
+    return a.first < b.first;
+}
+
+int min_rooms(vector<vector<int>> times) {
+    int n = times.size();
+    vector<pair<int, int>> timeline;
+
+    for (int i = 0; i < n; ++i) {
+        timeline.push_back({times[i][0], +1});
+        timeline.push_back({times[i][1], -1});
+    }
+
+    sort(timeline.begin(), timeline.end(), comp);
+
+    int current_meeting = 0;
+    int max_meeting = 0;
+
+    for (int i = 0; i < (2*n); ++i) {
+        current_meeting += timeline[i].second;
+        max_meeting = max(max_meeting, current_meeting);
+    }
+
+    return max_meeting;
+}
+
+int main() {
+    cout<<min_rooms({{0,30},{5,10},{15,20}})<<endl;
+    cout<<min_rooms({{1,18},{18,23},{15,29},{4,15},{2,11},{5,13}})<<endl;
+}
