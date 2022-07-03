498eef76f9bd31f742d5118abce19bfa40a15ca6
Mukesh Raj <mkrj@amazon.com>
2022-07-03 14:28:30 +0530

    Class 17

--- Class-17/fractional_knapsack.cpp
+++ Class-17/fractional_knapsack.cpp
@@ -3,3 +3,39 @@
 
 using namespace std;
 
+bool comp(pair<int, int> &a, pair<int, int> &b) {
+    return (a.first/(float)a.second) > (b.first/(float)b.second);
+}
+
+float fractional_knapsack(vector<int> values, vector<int> weights, int W) {
+
+    int n = values.size();
+    vector<pair<int, int>> items(n);
+
+    for (int i = 0; i < n; ++i) {
+        items[i] = {values[i], weights[i]};
+    }
+
+    // Sorting the items in decreasing order
+    sort(items.begin(), items.end(), comp);
+
+    float max_value = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (items[i].second <= W) {
+            max_value += items[i].first;
+            W -= items[i].second;
+        } else {
+            max_value += (items[i].first * W) / (float) (items[i].second);
+            W = 0;
+            break;
+        }
+    }
+
+    return max_value;
+}
+
+int main() {
+    cout<<fractional_knapsack({100,60,120},{20,10,30},50)<<endl;
+    cout<<fractional_knapsack({5,7,100},{10,40,45},50)<<endl;
+}
--- Class-17/maximise_tasks.cpp
+++ Class-17/maximise_tasks.cpp
@@ -3,3 +3,21 @@
 
 using namespace std;
 
+int get_max_task(vector<int> times, int total_time) {
+
+    sort(times.begin(), times.end());
+
+    for (int i = 0; i < times.size(); ++i) {
+        if (times[i] <= total_time) {
+            total_time -= times[i];
+        } else {
+            return i;
+        }
+    }
+    return times.size();
+}
+
+int main() {
+    cout<<get_max_task({4,2,1,2,5},8)<<endl;
+    cout<<get_max_task({3,2,1,2,5},8)<<endl;
+}
