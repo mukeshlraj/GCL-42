efc5005e4b78bb3695223a95e32bb8aa41e05bb3
Mukesh Raj <mkrj@amazon.com>
2022-07-10 16:42:58 +0530

    Classs 19

--- Class-19/maxi_path_tree.cpp
+++ Class-19/maxi_path_tree.cpp
@@ -3,3 +3,51 @@
 
 using namespace std;
 
+class TreeNode {
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int d) {
+        data = d;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+// TC : O(n)
+int linear_path_sum(TreeNode *root, int &result) {
+
+    if (root == NULL)
+        return 0;
+
+    int left_sum = linear_path_sum(root->left, result);
+    int right_sum = linear_path_sum(root->right, result);
+
+    result = max(result, max(0, left_sum) + max(0, right_sum) + root->data);
+
+    return max({root->data, left_sum+root->data, right_sum+root->data});
+}
+
+int max_sum_path(TreeNode *root) {
+    int result = INT_MIN;
+    linear_path_sum(root, result);
+    return result;
+}
+
+int main() {
+
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    cout << max_sum_path(root)<< endl;
+
+    TreeNode *root1 = new TreeNode(-10);
+    root1->left = new TreeNode(-20);
+    root1->right = new TreeNode(-30);
+
+    cout << max_sum_path(root1) << endl;
+
+}
--- Class-19/optimal_stratagy_game.cpp
+++ Class-19/optimal_stratagy_game.cpp
@@ -3,3 +3,33 @@
 
 using namespace std;
 
+vector<vector<int>> memo;
+
+int max_score_helper(vector<int> &A, int i, int j) {
+    if (i > j) {
+        return 0;
+    }
+
+    if (memo[i][j] != -1)
+        return memo[i][j];
+
+    // CASE 1 : Choose A[i]
+    int result_1 = A[i] + min(max_score_helper(A, i+2, j), max_score_helper(A,i+1, j-1));
+
+    // CASE 2 : Choose A[j]
+    int result_2 = A[j] + min(max_score_helper(A,i+1, j-1), max_score_helper(A, i, j-2));
+
+    return memo[i][j] = max(result_1, result_2);
+}
+
+int max_score(vector<int> A) {
+
+    int n = A.size();
+    memo = vector<vector<int>> (n, vector<int> (n, -1));
+    return max_score_helper(A, 0, A.size() - 1);
+}
+
+int main() {
+    cout << max_score({5,3,7,10}) << endl;
+    cout << max_score({8,15,3,7}) << endl;
+}
