01bac8cfa023ee8c10299dbc023cf54b3ba00d2d
Mukesh Raj <mkrj@amazon.com>
2022-07-10 14:44:22 +0530

    first 2 problems

--- Class-19/count_paths.cpp
+++ Class-19/count_paths.cpp
@@ -3,3 +3,54 @@
 
 using namespace std;
 
+// TC : O(n * m)
+// Aux Space : O(n * m)
+int count_path(vector<vector<bool>> mat) {
+    int n = mat.size();
+    int m = mat[0].size();
+
+    if (mat[0][0] == 1 or mat[n-1][m-1] == 1)
+        return 0;
+
+    vector<vector<int>> dp(n, vector<int>(m));
+    dp[0][0] = 1;
+
+    // Fill the 0th row
+    int j = 1;
+    while (j<m and mat[0][j] == 0) {
+        dp[0][j] = 1;
+        j++;
+    }
+    while (j<m) {
+        dp[0][j] = 0;
+        j++;
+    }
+
+    // Fill 0th column
+    int i=1;
+    while (i<n and mat[i][0] == 0) {
+        dp[i][0] = 1;
+        i++;
+    }
+    while (i<n) {
+        dp[i][0] = 0;
+        i++;
+    }
+
+    // Fill internal matrix
+    for(i = 1; i<n; i++) {
+        for(j=1; j<m; j++) {
+            if (mat[i][j] == 1)
+                dp[i][j] = 0;
+            else
+                dp[i][j] = dp[i][j-1] + dp[i-1][j];
+        }
+    }
+
+    return dp[n-1][m-1];
+}
+
+int main() {
+    cout<< count_path({{0,0,0},{0,1,0},{0,0,0}}) << endl;
+    cout<< count_path({{0,1,0,1},{0,0,0,0},{0,0,0,0},{0,1,1,0}}) << endl;
+}
--- Class-19/edit_distance.cpp
+++ Class-19/edit_distance.cpp
@@ -3,3 +3,48 @@
 
 using namespace std;
 
+vector<vector<int>> memo;
+
+// TC : O(n * m)
+// Aux Space : O(n * m)
+int get_min_helper(string &A, string &B, int i, int j) {
+    // base cases
+    if (i == A.length()) { // insert all the remaining char present in B
+        return B.length() - j;
+    }
+
+    if (j == B.length()) { // delete all the remaining char in A
+        return A.length() - i;
+    }
+
+    if (memo[i][j] != -1)
+        return memo[i][j];
+
+    if (A[i] == B[j]) {
+        return get_min_helper(A, B, i+1, j+1);
+    }
+
+    // CASE 1 : Try insertion
+    int result = 1 + get_min_helper(A, B, i, j+1);
+
+    // CASE 2 : Try deletion
+    result = min(result, 1 + get_min_helper(A, B, i+1, j));
+
+    // CASE 3 : Try replace
+    result = min(result, 1 + get_min_helper(A, B, i+1, j+1));
+
+    return memo[i][j] = result;
+}
+
+int get_min(string A, string B) {
+
+    int n = A.length();
+    int m = B.length();
+    memo = vector<vector<int>> (n, vector<int> (m, -1));
+    return get_min_helper(A, B, 0, 0);
+}
+
+int main() {
+    cout << get_min("abad", "abac") << endl;
+    cout << get_min("Anshuman", "Antihuman") << endl;
+}
